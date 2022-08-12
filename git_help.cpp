3755e6ea14b89ff9b0e77bd633ecbb4db1566a7c
Suryadev25 <suryads5052@gmail.com>
2022-08-12 07:59:11 +0530

    code

---
+++ git_help.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int int64_t
+using namespace std;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << "HELP";
+    return 0;
+}
